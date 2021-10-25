95ee28c68df6a6beb1c617097e519453e9463a66
Xuanteo <81322256+Xuanteo@users.noreply.github.com>
2021-10-25 16:54:43 +0700

    add Assignment 3

---
+++ TestingSystem_Assignment_3/src/frontend/Exercise1_DatatypeCasting.java
@@ -0,0 +1,83 @@
+package frontend;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Exercise1_DatatypeCasting {
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Mời ban chọn câu hỏi: ");
+		System.out.println("Nhập vào số 1 để thực hiện Question1:");
+		System.out.println("Nhập vào số 2 để thực hiện Question2:");
+		System.out.println("Nhập vào số 3 để thực hiện Question3:");
+		System.out.println("Nhập vào số 4 để thực hiện Question3:");
+		int choose = sc.nextInt();
+		switch (choose) {
+		case 1:
+			question1();
+			break;
+		case 2:
+			question2();
+			break;
+		case 3:
+			question3();
+			break;
+		case 4:
+			question4();
+			break;
+
+		default:
+			break;
+		}
+	}
+
+//	Question 4:
+//		Viết 1 method nhập vào 2 số nguyên a và b và trả về thương của chúng
+	private static void question4() {
+		// TODO Auto-generated method stub
+
+	}
+
+//	Question 3:
+//		Lấy 2 số cuối của số ở Question 2 và in ra.
+//		Gợi ý:
+//		Cách 1: convert số có 5 chữ số ra String, sau đó lấy 2 số cuối
+//		Cách 2: chia lấy dư số đó cho 100
+	private static void question3() {
+		// TODO Auto-generated method stub
+
+	}
+
+//	Question 2:
+//		Lấy ngẫu nhiên 1 số có 5 chữ số (những số dưới 5 chữ số thì sẽ thêm
+//		có số 0 ở đầu cho đủ 5 chữ số)
+	private static void question2() {
+		System.out.println("Lấy ngẫy nhiên 1 số có 5 chữ số: ");
+		int min = 0;
+		int max = 99999;
+		int a = (int) (Math.random() * max) + min;
+		if (a < 10000) {
+			a = a* 10;
+		}
+		System.out.println("Số ngẫu nhiên: " + a);
+	}
+
+//	Exercise 1 (Optional): Datatype Casting
+//	Question 1:
+//	Khai báo 2 số lương có kiểu dữ liệu là float.
+//	Khởi tạo Lương của Account 1 là 5240.5 $
+//	Khởi tạo Lương của Account 2 là 10970.055$
+//	Khai báo 1 số int để làm tròn Lương của Account 1 và in số int đó ra
+//	Khai báo 1 số int để làm tròn Lương của Account 2 và in số int đó ra
+	private static void question1() {
+		float accsalary1;
+		float accsalary2;
+		accsalary1 = (float) 5240.5;
+		accsalary2 = (float) 10970.055;
+		System.out.println("Account salary 1: " + accsalary1 + " || Account salary 2:" + accsalary2);
+		int roundSalary1 = (int) accsalary1;
+		int roundSalary2 = (int) accsalary2;
+		System.out.println("Round Account 1: " + roundSalary1 + "|| Round Account 2: " + roundSalary2);
+	}
+}
